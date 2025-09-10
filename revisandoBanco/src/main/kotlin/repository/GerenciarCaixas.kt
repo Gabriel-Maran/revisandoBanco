475fa14d25cc6a9f2a3664c3220c4a49bf2c9c37
Gabriel Maran <gabriel.enzo.libero.maran@gmail.com>
2025-09-09 21:14:32 -0300

    Terminando CRUD
    Terminando base do CRUD, sem verificações grandes

--- revisandoBanco/src/main/kotlin/repository/GerenciarCaixas.kt
+++ revisandoBanco/src/main/kotlin/repository/GerenciarCaixas.kt
@@ -11,12 +11,58 @@ val conectar = EntidadeJDBC(
         senha = "root"
     )
 
+fun criarTabelaCaixa(){
 
+    val sql = "CREATE TABLE IF NOT EXISTS CaixaDAgua (" +
+            "id serial NOT NULL PRIMARY KEY, " +
+            "material varchar(255) NOT NULL, " +
+            "capacidade int NOT NULL, " +
+            "peso float NOT NULL, " +
+            "marca varchar(255) NOT NULL, " +
+            "cor varchar(255) NOT NULL, " +
+            "tampa boolean NOT NULL, " +
+            "preco varchar(255) NOT NULL" +
+            ");"
+    val banco = conectar.connectarComBanco()
+    val enviarParaBanco = banco!!.createStatement().execute(sql)
+
+    println(enviarParaBanco)
+    banco.close()
+}
+
+fun listarById(){
+    var id = ""
+    val regex = "^[1-9]\\d*|0\$"
+    do{
+        println("Digite o ID que deseja listar")
+        id = readlnOrNull() ?: "a"
+    }while(id == regex)
+    if(id == "-1"){
+        println("ID invalido")
+        return
+    }
+    val sql = "SELECT * FROM CaixaDAgua Where id = ?"
+    val stmt = conectar.connectarComBanco()!!.prepareStatement(sql)
+    stmt.setInt(1, id.toInt())
+    val resp = stmt.executeQuery()
+    println("[")
+    while(resp.next()){
+        val id = resp.getInt("id")
+        val material = resp.getString("material")
+        val capacidade = resp.getFloat("capacidade")
+        val peso = resp.getString("marca")
+        val cor = resp.getString("cor")
+        val tampa = resp.getBoolean("tampa")
+        val preco = resp.getString("preco")
+        println("{id = $id, material = $material, capacidade = $capacidade, peso = $peso, cor = $cor, tampa = $tampa, preco = $preco}")
+    }
+    println("]")
+}
 
 fun createCaixa() {
     /*capacidade: Int,*/
     println("Digite a capacidade da caixa d'água")
-    val capacidade: Int = readln().toInt()
+    val capacidade: Int = readln().toIntOrNull() ?:0
 
     /*material: Material*/
     println("Digite o material da caixa d'água")
@@ -37,15 +83,15 @@ fun createCaixa() {
 
     /*peso: Double*/
     println("Qual o peso da caixa d'água?")
-    val peso: Double = readln().toDouble()
+    val peso: Double = readln().toDoubleOrNull() ?: 0.0
 
     /*marca: String,*/
     println("Qual a marca da caixa d'água")
-    val marca:String = readln()
+    val marca:String = readlnOrNull() ?: ""
 
     /*cor: String*/
     println("Qual a cor da caixa d'água")
-    val cor:String = readln()
+    val cor:String = readlnOrNull() ?: ""
 
     /*tampa: Boolean*/
     println("Tem tampa?(1 = sim, 2 = não)")
@@ -57,7 +103,7 @@ fun createCaixa() {
 
     /*preco: BigDecimal*/
     println("Qual o preço da caixa d'água")
-    val preco:BigDecimal = readln().toBigDecimal()
+    val preco:BigDecimal = (readlnOrNull()?.toBigDecimal() ?: "0.0") as BigDecimal
 
     val novaCaixa: CaixaDAgua = CaixaDAgua(
         capacidade = capacidade,
@@ -87,27 +133,85 @@ fun createCaixa() {
     banco.close()
 }
 
-fun criarTabelaCaixa(){
-
-    val sql = "CREATE TABLE IF NOT EXISTS CaixaDAgua (" +
-            "id serial NOT NULL PRIMARY KEY, " +
-            "material varchar(255) NOT NULL, " +
-            "capacidade int NOT NULL, " +
-            "peso float NOT NULL, " +
-            "marca varchar(255) NOT NULL, " +
-            "cor varchar(255) NOT NULL, " +
-            "tampa boolean NOT NULL, " +
-            "preco varchar(255) NOT NULL" +
-            ");"
-    val banco = conectar.connectarComBanco()
-    val enviarParaBanco = banco!!.createStatement().execute(sql)
-
-    println(enviarParaBanco)
-    banco.close()
-}
-
 fun editarCaixa() {
+    listarCaixas()
+    println("Qual o id da caixa que você deseja editar?")
+    val escolhaId = readln().toIntOrNull() ?: 0
+    val atributo = arrayOf("material", "capacidade", "marca", "cor", "tampa", "preco", "peso")
+    println("1- material, 2-capacidade, 3-marca, 4-cor, 5-tampa, 6-preço, 7-peso")
+    println("Qual atributo deseja trocar?")
+    var novoAtributo = "";
+    val atributoEscolhido = readlnOrNull() ?: "0"
+    val sql = "UPDATE CaixaDAgua SET ${atributo[atributoEscolhido.toInt()-1]} = ? WHERE id = ?"
 
+    val banco = conectar.connectarComBanco()
+    val stmt = banco!!.prepareStatement(sql)
+    stmt.setInt(2, escolhaId)
+    when(atributoEscolhido){
+        "1" ->{
+            println("Digite o novo material da caixa d'água")
+            println("1 - POLIETILENO")
+            println("2 - FIBRADEVIDRO")
+            println("3 - ACOINOXIDAVEL")
+            println("4 - CONCRETO")
+            println("5 - OUTRO")
+
+            novoAtributo = when (readln()) {
+                "1" -> Material.POLIETILENO.toString()
+                "2" -> Material.FIBRADEVIDRO.toString()
+                "3" -> Material.ACOINOXIDAVEL.toString()
+                "4" -> Material.CONCRETO.toString()
+                "5" -> Material.OUTRO.toString()
+                else -> Material.OUTRO.toString()
+            }
+            stmt.setString(1, novoAtributo)
+        }
+        "2" ->{
+            println("Digite a nova capacidade da caixa d'água")
+            novoAtributo = readlnOrNull() ?: "0"
+            stmt.setFloat(1, novoAtributo.toFloat())
+        }
+        "3" ->{
+            println("Qual a nova marca da caixa d'água")
+            novoAtributo  = readlnOrNull() ?: "Sem Marca"
+
+            stmt.setString(1, novoAtributo)
+        }
+        "4" ->{
+            println("Qual o novo cor da caixa d'água?")
+            novoAtributo = readlnOrNull() ?: "0"
+
+            stmt.setString(1, novoAtributo)
+        }
+        "5" ->{
+            println("Agora tem tampa?(1 = sim, 2 = não)")
+            novoAtributo = when (readln().toInt()) {
+                1 -> "true"
+                2 -> "false"
+                else -> "false"
+            }
+
+            stmt.setBoolean(1, novoAtributo.toBoolean())
+        }
+        "6" ->{
+            println("Qual o novo preço da caixa d'água")
+            novoAtributo = readlnOrNull() ?: "0"
+
+            stmt.setString(1, novoAtributo)
+        }
+        "7"->{
+            println("Qual o novo peso da caixa?")
+            novoAtributo = readlnOrNull() ?: "0"
+
+            stmt.setFloat(1, novoAtributo.toFloat())
+        }
+        else ->{
+            println("Escolha invalida")
+            return
+        }
+    }
+    stmt.executeUpdate()
+    banco.close()
 }
 
 fun listarCaixas() {
@@ -115,7 +219,6 @@ fun listarCaixas() {
     val banco = conectar.connectarComBanco()
     val stmt = banco!!.createStatement()
     val resp = stmt.executeQuery(sql)
-    println("[")
     while(resp.next()){
         val id = resp.getInt("id")
         val material = resp.getString("material")
@@ -126,7 +229,6 @@ fun listarCaixas() {
         val preco = resp.getString("preco")
         println("{id = $id, material = $material, capacidade = $capacidade, peso = $peso, cor = $cor, tampa = $tampa, preco = $preco}")
     }
-    println("]")
     banco.close()
 }
 
@@ -144,4 +246,5 @@ fun deleteCaixas() {
     }else{
         println("Id não encontrado")
     }
+    banco.close()
 }
--- revisandoBanco/src/main/kotlin/ui/Menu.kt
+++ revisandoBanco/src/main/kotlin/ui/Menu.kt
@@ -1,8 +1,6 @@
 package ui
 
-import repository.createCaixa
-import repository.deleteCaixas
-import repository.listarCaixas
+import repository.*
 
 fun menu() {
     var opcao = 0
@@ -10,16 +8,18 @@ fun menu() {
         println("1- Cadastrar Caixas D'Água")
         println("2- Editar Caixas D'Água")
         println("3- Listar Caixas D'Água")
-        println("4- Excluir Caixas D'Água")
+        println("4- Listar Caixa D'Água por Id")
+        println("5- Excluir Caixas D'Água")
         println("0- Sair")
         opcao = readln().toInt()
 
         when (opcao) {
             0 -> println("Saindo....")
             1 -> createCaixa()
-            2 -> println("Editando caixa...")
+            2 -> editarCaixa()
             3 -> listarCaixas()
-            4 -> deleteCaixas()
+            4 -> listarById()
+            5 -> deleteCaixas()
             else -> println("Opção inválida")
         }
     } while (opcao != 0)
